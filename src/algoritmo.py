e658425ab806c8c1a3aa156b8b3b5010d2ea30e0
laurabeatriz <laurabeatriz.or@gmail.com>
2026-07-16 18:31:33 -0300

    Refatora algoritmo em função reutilizável

--- src/algoritmo.py
+++ src/algoritmo.py
@@ -1,2 +1,17 @@
 def gerar_receita_linha(linha):
-    pass
+    receita = []
+    cor_atual = linha[0]
+    contador = 1
+    
+    for i in range(1,len(linha)):
+        if linha[i]== cor_atual:
+            contador+=1
+
+        else: 
+            receita.append(str(contador) +"x "+ cor_atual)
+            cor_atual = linha[i]
+            contador = 1
+        
+    receita.append(str(contador) +"x "+ cor_atual)
+    #técnica run-length Encoding-RLE
+    return ", ".join(receita)
--- src/main.py
+++ src/main.py
@@ -4,26 +4,13 @@ import io
 # Força o terminal a usar UTF-8 para exibir os textos corretamente
 sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
-#from algoritmo import gerar_receita_linha
+from algoritmo import gerar_receita_linha
 
 
 
 linha = ["preta", "preta", "azul", "azul", "azul", "preta"]
 
 
-cor_atual = linha[0]
-contador = 1
+resultado = gerar_receita_linha(linha)
 
-for i in range(1,len(linha)):
-    if linha[i]== cor_atual:
-        contador+=1
-
-    else: 
-        print(str(contador) +"x "+ cor_atual)
-        cor_atual = linha[i]
-        contador = 1
-        
-print(str(contador) + "x " + cor_atual)
-
-#técnica run-length Encoding-RLE
-
+print(resultado)
